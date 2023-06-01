c3031308a66a4d07895bc2b81049068b0ac126f4
net_creature <agustinmediotti@gmail.com>
2023-06-01 19:43:22 -0300

    added dummy data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 
@@ -65,5 +68,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* DUMMY DATA (execute one time only!) */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((err) => console.log(`${err} did not connect`));
