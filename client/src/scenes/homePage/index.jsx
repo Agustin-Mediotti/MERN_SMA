3c4a0af6930726c0ea40646941d187427f4bbe47
net_creature <agustinmediotti@gmail.com>
2023-06-09 15:25:42 -0300

    added MyPostWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,6 +3,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "scenes/navbar";
 import { useSelector } from "react-redux";
 import UserWidget from "scenes/widgets/UserWidget.jsx";
+import MyPostWidget from "scenes/widgets/MyPostWidget.jsx";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
@@ -25,7 +26,7 @@ const HomePage = () => {
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
-          {/* widgets */}
+          <MyPostWidget picturePath={picturePath} />
         </Box>
         {isNonMobileScreens && <Box flexBasis="26%">{/* widgets */}</Box>}
       </Box>
